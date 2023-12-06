2fb6d07a68b459dffee1f593f5a0e2994b63fa11
DESKTOP-U8SGGL9\devru <dev.ruben.lopez@outlook.com>
2023-12-06 21:59:10 +1030

    First steps

---
+++ MuManClassLib/MusicManagerConfiguration.cs
@@ -0,0 +1,11 @@
+
+
+namespace MusicManagerClassLib;
+
+
+public class Mp3LibraryConfiguration
+{
+    public string SourcePath { get; set; }
+    public string DestinationRoot { get; set; }
+    public string[] FolderStructureTags { get; set; }
+}

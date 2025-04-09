3463b92da46dffb7a5b9b3768c0a7cfb60985222
Ruben Lopez <dev.ruben.lopez@outlook.com>
2025-04-09 13:47:31 +0930

    Update Helpers.cs

--- Helpers.cs
+++ Helpers.cs
@@ -19,3 +19,41 @@ public static class OperatorHelper
 bool result1 = OperatorHelper.Compare(10, 20, "<");   // true
 bool result2 = OperatorHelper.Compare("apple", "banana", "!="); // true
 bool result3 = OperatorHelper.Compare(5.0, 5.0, "=="); // true
+
+
+using System.Text.Json.Nodes;
+
+public static class JsonNodeCaster
+{
+    public static void ExtractValue(JsonNode node, out string? strValue, out double? numValue)
+    {
+        strValue = null;
+        numValue = null;
+
+        if (node is JsonValue valueNode)
+        {
+            var val = valueNode.GetValue<object>();
+
+            switch (val)
+            {
+                case string s:
+                    strValue = s;
+                    break;
+
+                case int i:
+                case long l:
+                case float f:
+                case double d:
+                case decimal m:
+                    numValue = Convert.ToDouble(val);
+                    break;
+
+                default:
+                    // Handle other types if needed
+                    break;
+            }
+        }
+    }
+}
+
+
